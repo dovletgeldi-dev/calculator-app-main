da4c7f2f1d8de8d46f93e4c7d80f6a676d085a44
Dovlet <dovlethelegend@gmail.com>
2023-04-11 23:28:30 +0800

    added the main functionalities such as clear  and delete screen as well as calculation

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 "use strict";
 
-let firstNumber = 0;
-let secondNumber = 0;
-let currentOperator = "";
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = null;
+let shouldClearScreen = false;
 
 const numbersBtn = document.querySelectorAll("#number");
 const operatorsBtn = document.querySelectorAll("#operator");
 const equalsBtn = document.querySelector(".equal");
-const clearBtn = document.querySelector(".clear");
+const resetBtn = document.querySelector(".reset");
 const deleteBtn = document.querySelector("#deleteNum");
 const dotBtn = document.querySelector(".dot");
 const previousOperationDisplay = document.querySelector(".previousOperation");
 const currentOperationDisplay = document.querySelector(".currentOperation");
+
+resetBtn.addEventListener("click", () => resetScreen());
+equalsBtn.addEventListener("click", () => evaluateOperation());
+deleteBtn.addEventListener("click", () => deleteNumber());
+
+const clearScreen = () => {
+  currentOperationDisplay.textContent = "";
+  shouldClearScreen = false;
+};
+
+const resetScreen = () => {
+  currentOperationDisplay.textContent = "0";
+  previousOperationDisplay.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = null;
+};
+
+const deleteNumber = () => {
+  currentOperationDisplay.textContent = currentOperationDisplay.textContent
+    .toString()
+    .slice(0, -1);
+};
 
 numbersBtn.forEach((e) => {
   e.addEventListener("click", () => populateNumber(e.textContent));
@@ -21,8 +45,38 @@ operatorsBtn.forEach((e) => {
   e.addEventListener("click", () => populateOperator(e.textContent));
 });
 
-const populateNumber = (number) => {
-  currentOperationDisplay.textContent += number;
+const populateNumber = (num) => {
+  if (currentOperationDisplay.textContent === "0" || shouldClearScreen)
+    clearScreen();
+
+  currentOperationDisplay.textContent += num;
+};
+
+const populateOperator = (opr) => {
+  if (currentOperator !== null) evaluateOperation();
+  firstNumber = currentOperationDisplay.textContent;
+  currentOperator = opr;
+  previousOperationDisplay.textContent = `${firstNumber} ${currentOperator}`;
+  shouldClearScreen = true;
+};
+
+const evaluateOperation = () => {
+  if (currentOperator === null || shouldClearScreen) return;
+  if (currentOperator === "÷" && currentOperationDisplay.textContent === "0") {
+    alert("You can't divide 0 by 0 ....");
+    resetScreen();
+    return;
+  }
+  secondNumber = currentOperationDisplay.textContent;
+  currentOperationDisplay.textContent = roundCalculation(
+    operate(currentOperator, firstNumber, secondNumber)
+  );
+  previousOperationDisplay.textContent = `${firstNumber} ${currentOperator} ${secondNumber}`;
+  currentOperator = null;
+};
+
+const roundCalculation = (num) => {
+  return Math.round(num * 100) / 100;
 };
 
 const add = (a, b) => {
@@ -42,21 +96,22 @@ const divide = (a, b) => {
 };
 
 const operate = (operator, a, b) => {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
-      console.log(add(a, b));
-      break;
+      return add(a, b);
     case "-":
-      console.log(subtract(a, b));
-      break;
-    case "*":
-      console.log(multiply(a, b));
-      break;
-    case "/":
-      console.log(divide(a, b));
-      break;
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) {
+        return null;
+      } else {
+        return divide(a, b);
+      }
     default:
+      return null;
   }
 };
-
-operate(currentOperator, firstNumber, secondNumber);
